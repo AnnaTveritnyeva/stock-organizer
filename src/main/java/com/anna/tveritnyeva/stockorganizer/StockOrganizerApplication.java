06df6f3ba8fd03dee2dfe4f358839d7630ce7bf1
AnnaTveritnyeva <atveretniv@gmail.com>
2022-10-07 16:29:41 +0300

    Initial commit

---
+++ src/main/java/com/anna/tveritnyeva/stockorganizer/StockOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.anna.tveritnyeva.stockorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockOrganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anna/tveritnyeva/stockorganizer/StockOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anna.tveritnyeva.stockorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
