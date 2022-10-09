788ef1d39f2edf333f86038377d0deee31a3b0a6
AnnaTveritnyeva <atveretniv@gmail.com>
2022-10-09 18:39:58 +0300

    Created Item Document Class

---
+++ src/main/java/com/anna/tveritnyeva/stockorganizer/documents/Item.java
@@ -0,0 +1,47 @@
+package com.anna.tveritnyeva.stockorganizer.documents;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Document(collection = "items")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Item {
+    @Id
+    private String id;
+    @Field(name = "user_id")
+    private String userId;
+    private String name;
+    private boolean missing;
+
+    @Field(name = "store_URLs")
+    @Singular
+    private List<String> storeURLs;
+
+    @Field(name = "organization_hierarchy")
+    @Singular("section")
+    private List<String> organizationHierarchy;
+
+    public Item(String userId, String name, boolean missing) {
+        this.userId = userId;
+        this.name = name;
+        this.missing = missing;
+        this.storeURLs = new ArrayList<>();
+        this.organizationHierarchy = new ArrayList<>();
+    }
+
+    public Item(String userId, String name) {
+        this.userId = userId;
+        this.name = name;
+        this.missing = false;
+        this.storeURLs = new ArrayList<>();
+        this.organizationHierarchy = new ArrayList<>();
+    }
+}
