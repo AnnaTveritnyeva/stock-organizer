c4ffb86764ba659f38c6bb59db340ab11da1e4eb
AnnaTveritnyeva <atveretniv@gmail.com>
2022-10-10 12:11:23 +0300

    Converted storeUrls in Item from List<String> to List<StoreUrl> and created StoreUrl object

---
+++ src/main/java/com/anna/tveritnyeva/stockorganizer/beans/StoreUrl.java
@@ -0,0 +1,13 @@
+package com.anna.tveritnyeva.stockorganizer.beans;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StoreUrl {
+    private String storeName;
+    private String storeUrl;
+}
--- src/main/java/com/anna/tveritnyeva/stockorganizer/documents/Item.java
+++ src/main/java/com/anna/tveritnyeva/stockorganizer/documents/Item.java
@@ -1,5 +1,6 @@
 package com.anna.tveritnyeva.stockorganizer.documents;
 
+import com.anna.tveritnyeva.stockorganizer.beans.StoreUrl;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -23,7 +24,7 @@ public class Item {
 
     @Field(name = "store_URLs")
     @Singular
-    private List<String> storeURLs;
+    private List<StoreUrl> storeURLs;
 
     @Field(name = "organization_hierarchy")
     @Singular("section")
