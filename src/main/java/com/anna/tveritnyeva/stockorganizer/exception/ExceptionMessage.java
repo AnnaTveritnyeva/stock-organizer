05ec2aad2ac35383695166b93a058ce9239854b5
AnnaTveritnyeva <atveretniv@gmail.com>
2022-10-18 18:31:44 +0300

    Added Category exception messages

--- src/main/java/com/anna/tveritnyeva/stockorganizer/exception/ExceptionMessage.java
+++ src/main/java/com/anna/tveritnyeva/stockorganizer/exception/ExceptionMessage.java
@@ -2,8 +2,15 @@ package com.anna.tveritnyeva.stockorganizer.exception;
 
 public enum ExceptionMessage {
     EXCEPTION_MESSAGE("EXCEPTION MESSAGE: "),
+
+    //Item exceptions:
     ITEM_ALREADY_EXISTS("item already exists."),
-    ITEM_DOES_NOT_EXIST("item doesn't exist.");
+    ITEM_DOES_NOT_EXIST("item doesn't exist."),
+
+    //Category exceptions:
+    CATEGORY_NAME_ALREADY_EXIST("category name already exist"),
+    CATEGORY_DOES_NOT_EXIST("category doesn't exist"),
+    SECTION_ALREADY_EXISTS("section already exists");
 
     private final String msg;
 
