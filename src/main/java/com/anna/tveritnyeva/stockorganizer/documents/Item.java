bce5f7268399aa5d366fcfa74c772806ed2ec873
AnnaTveritnyeva <atveretniv@gmail.com>
2022-10-18 18:34:07 +0300

    Updated organizationHierarchy field to be type of OrganizationHierarchy.java and added purchaseHistory field

--- src/main/java/com/anna/tveritnyeva/stockorganizer/documents/Item.java
+++ src/main/java/com/anna/tveritnyeva/stockorganizer/documents/Item.java
@@ -1,11 +1,13 @@
 package com.anna.tveritnyeva.stockorganizer.documents;
 
+import com.anna.tveritnyeva.stockorganizer.beans.OrganizationHierarchy;
 import com.anna.tveritnyeva.stockorganizer.beans.StoreUrl;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,22 +29,27 @@ public class Item {
     private List<StoreUrl> storeURLs;
 
     @Field(name = "organization_hierarchy")
-    @Singular("section")
-    private List<String> organizationHierarchy;
+    private OrganizationHierarchy organizationHierarchy;
 
-    public Item(String userId, String name, boolean missing) {
+    @Singular("purchase_date")
+    @Field("purchase_history")
+    private List<LocalDate> purchaseHistory;
+
+    public Item(String userId, String name, boolean missing, OrganizationHierarchy organizationHierarchy) {
         this.userId = userId;
         this.name = name;
         this.missing = missing;
+        this.organizationHierarchy = organizationHierarchy;
         this.storeURLs = new ArrayList<>();
-        this.organizationHierarchy = new ArrayList<>();
+        this.purchaseHistory = new ArrayList<>();
     }
 
-    public Item(String userId, String name) {
+    public Item(String userId, String name, OrganizationHierarchy organizationHierarchy) {
         this.userId = userId;
         this.name = name;
         this.missing = false;
+        this.organizationHierarchy = organizationHierarchy;
         this.storeURLs = new ArrayList<>();
-        this.organizationHierarchy = new ArrayList<>();
+        this.purchaseHistory = new ArrayList<>();
     }
 }
